73cef4dc6695e2bf3dbfbec71333267d6a55841e
naolt <naoltamrat11@gmai.com>
2023-10-04 13:50:34 +0300

    committed

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -6,8 +6,12 @@ export async function POST(request: Request) {
   const res = await request.json();
   if (request.body) {
     await sendMail(res.name, res.email, res.message);
+    return NextResponse.json({ message: "Success" });
   }
-  return NextResponse.json({ message: "Success" });
+  return new Response(null, {
+    status: 500,
+    statusText: "INTERNAL_SERVER_ERROR",
+  });
 }
 
 //-----------------------------------------------------------------------------
